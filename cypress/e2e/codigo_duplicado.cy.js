9bc5f749d4d2be2230a8706f177312202c0feb0f
LeaLeg <leandro.legaria@gmail.com>
2025-09-03 11:04:32 -0300

    actualizacion clase 5

--- cypress/e2e/codigo_duplicado.cy.js
+++ cypress/e2e/codigo_duplicado.cy.js
@@ -5,25 +5,27 @@ describe('Codigo duplicado', () => {
     cy.intercept('GET','**/search**').as('getStories')
     cy.visit('https://hackernews-seven.vercel.app')
     cy.wait('@getStories')
-    cy.get('input[type="text"]').should('be.visible').and('have.value','redux').as ('searchField').clear()
-  });
+    cy.get('input[type="text"]').should('be.visible').as('searchField').and('have.value','redux').clear()
+    });
 
-  it('Buscar elemento vuejs', () => {
+  const searchTerms = ['vuejs', 'reactjs', 'test'];
+  searchTerms.forEach(term => {
+    it(`Buscar elemento ${term}`, () => {
+      cy.search(term)
+      cy.wait('@getStories')
+      cy.get('.table-row').should('have.length',100)
+    });
+  })
+
+  it.skip('Buscar elemento vuejs', () => {
     cy.get('@searchField').type('vuejs{enter}')
     cy.wait('@getStories')
     cy.get('.table-row').should('have.length',100)
   });
 
-  it('Buscar elemento reactjs', () => {
+  it.skip('Buscar elemento reactjs', () => {
     cy.get('@searchField').type('reactjs{enter}')
     cy.wait('@getStories')
     cy.get('.table-row').should('have.length',100)
   });
-
-  it('Buscar elemento test', () => {
-    cy.get('@searchField').type('test{enter}')
-    cy.wait('@getStories')
-    cy.get('.table-row').should('have.length',100)
-  });
-
-});
+})
